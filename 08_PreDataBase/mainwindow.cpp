7f312be5587c9c94d7f58d07ee611c5189058c31
twrnty12 <143382950+twrnty12@users.noreply.github.com>
2024-09-28 17:44:54 +0300

    mainwindow.cpp

--- 08_PreDataBase/mainwindow.cpp
+++ 08_PreDataBase/mainwindow.cpp
@@ -5,44 +5,36 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    //Исходное состояние виджетов
+
     ui->setupUi(this);
     ui->lb_statusConnect->setStyleSheet("color:red");
     ui->pb_request->setEnabled(false);
 
-    /*
-     * Выделим память под необходимые объекты. Все они наследники
-     * QObject, поэтому воспользуемся иерархией.
-    */
+
 
     dataDb = new DbData(this);
     dataBase = new DataBase(this);
     msg = new QMessageBox(this);
 
-    //Установим размер вектора данных для подключения к БД
+
     dataForConnect.resize(NUM_DATA_FOR_CONNECT_TO_DB);
 
-    /*
-     * Добавим БД используя стандартный драйвер PSQL и зададим имя.
-    */
+    
+    //Добавим БД
+
     dataBase->AddDataBase(POSTGRE_DRIVER, DB_NAME);
 
-    /*
-     * Устанавливаем данные для подключениея к БД.
-     * Поскольку метод небольшой используем лямбда-функцию.
-     */
+
+     // Устанавливаем данные для подключениея к БД.
+
+
     connect(dataDb, &DbData::sig_sendData, this, [&](QVector<QString> receivData){
         dataForConnect = receivData;
     });
 
-    /*
-     * Соединяем сигнал, который передает ответ от БД с методом, который отображает ответ в ПИ
-     */
+
     connect(dataBase, &::DataBase::sig_SendQueryFromDB, this, &MainWindow::ScreenQueryFromDB);
     connect(dataBase, &::DataBase::sig_SendTableFromDB, this, &MainWindow::ScreenTableFromDB);
-    /*
-     *  Сигнал для подключения к БД
-     */
     connect(dataBase, &DataBase::sig_SendStatusConnection, this, &MainWindow::ReceiveStatusConnectionToDB);
 
 }
@@ -52,27 +44,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-/*!
- * @brief Слот отображает форму для ввода данных подключения к БД
- */
+
 void MainWindow::on_act_addData_triggered()
 {
-    //Отобразим диалоговое окно. Какой метод нужно использовать?
+  
     dataDb->show();
 }
 
-/*!
- * @brief Слот выполняет подключение к БД. И отображает ошибки.
- */
 
 void MainWindow::on_act_connect_triggered()
 {
-    /*
-     * Обработчик кнопки у нас должен подключаться и отключаться от БД.
-     * Можно привязаться к надписи лейбла статуса. Если он равен
-     * "Отключено" мы осуществляем подключение, если "Подключено" то
-     * отключаемся
-    */
 
     if(ui->lb_statusConnect->text() == "Отключено"){
 
@@ -94,9 +75,9 @@ void MainWindow::on_act_connect_triggered()
 
 }
 
-/*!
- * \brief Обработчик кнопки "Получить"
- */
+
+ // "Получить"
+ 
 void MainWindow::on_pb_request_clicked()
 {
     QString category;
@@ -123,26 +104,19 @@ void MainWindow::on_pb_request_clicked()
 
 }
 
-/*!
- * \brief Слот отображает значение в QTableWidget
- * \param widget
- * \param typeRequest
- */
+
 void MainWindow::ScreenQueryFromDB(QSqlQueryModel *model)
 {
     ui->tb_result->setModel(model);
-    ///Тут должен быть код ДЗ
+  
 }
 
 void MainWindow::ScreenTableFromDB(QSqlTableModel *model)
 {
     ui->tb_result->setModel(model);
-    ///Тут должен быть код ДЗ
+  
 }
-/*!
- * \brief Метод изменяет стотояние формы в зависимости от статуса подключения к БД
- * \param status
- */
+
 void MainWindow::ReceiveStatusConnectionToDB(bool status)
 {
     if(status){
