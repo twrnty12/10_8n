91e0de3fef5eadcbae3afe9dc1aa0ecc64d1f982
twrnty12 <143382950+twrnty12@users.noreply.github.com>
2024-09-28 17:45:50 +0300

    database.cpp

--- 08_PreDataBase/database.cpp
+++ 08_PreDataBase/database.cpp
@@ -13,11 +13,7 @@ DataBase::~DataBase()
     delete dataBase;
 }
 
-/*!
- * \brief Метод добавляет БД к экземпляру класса QSqlDataBase
- * \param driver драйвер БД
- * \param nameDB имя БД (Если отсутствует Qt задает имя по умолчанию)
- */
+
 void DataBase::AddDataBase(QString driver, QString nameDB)
 {
     *dataBase = QSqlDatabase::addDatabase(driver, nameDB);
@@ -25,11 +21,7 @@ void DataBase::AddDataBase(QString driver, QString nameDB)
 
 }
 
-/*!
- * \brief Метод подключается к БД
- * \param для удобства передаем контейнер с данными необходимыми для подключения
- * \return возвращает тип ошибки
- */
+
 void DataBase::ConnectToDataBase(QVector<QString> data)
 {
 
@@ -39,7 +31,7 @@ void DataBase::ConnectToDataBase(QVector<QString> data)
     dataBase->setPassword(data[pass]);
     dataBase->setPort(data[port].toInt());
 
-    ///Тут должен быть код ДЗ
+
 
 
     bool status;
@@ -47,10 +39,7 @@ void DataBase::ConnectToDataBase(QVector<QString> data)
     emit sig_SendStatusConnection(status);
 
 }
-/*!
- * \brief Метод производит отключение от БД
- * \param Имя БД
- */
+
 void DataBase::DisconnectFromDataBase(QString nameDb)
 {
 
@@ -58,11 +47,7 @@ void DataBase::DisconnectFromDataBase(QString nameDb)
     dataBase->close();
 
 }
-/*!
- * \brief Метод формирует запрос к БД.
- * \param request - SQL запрос
- * \return
- */
+
 void DataBase::RequestToDB(QString request)
 {
     ///Тут должен быть код ДЗ
@@ -83,9 +68,7 @@ void DataBase::RequestToDB(QString request)
     }
 }
 
-/*!
- * @brief Метод возвращает последнюю ошибку БД
- */
+
 QSqlError DataBase::GetLastError()
 {
     return dataBase->lastError();
